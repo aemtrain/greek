df12acddd75a08f948c683c2403d8922b2316605
user <user@ManiSiri.mshome.net>
2018-09-16 18:10:23 +0530

    Mechi - MultiField Component

---
+++ sampletraining/core/src/main/java/org/aemtrain/sampletraining/core/components/MultiFieldUse.java
@@ -0,0 +1,12 @@
+package org.aemtrain.sampletraining.core.components;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+
+public class MultiFieldUse extends WCMUsePojo {
+
+	@Override
+	public void activate() throws Exception {
+			
+	}
+
+}
